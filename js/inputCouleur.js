023a776de47b0a6413832408a3ff1347bb17d897
kenuhn <kenuhn@gmail.com>
2023-01-16 00:24:49 +0100

    erreur doctype

--- js/inputCouleur.js
+++ js/inputCouleur.js
@@ -1,70 +1,64 @@
 
+
 function animationBtnTag(data) {
-    const input = document.querySelectorAll('.mots-clés-input')
-    const arrowUp = document.querySelectorAll('.arrow-up')
-    const arrowDown = document.querySelectorAll('.arrow-down')
-    const texteInput = document.querySelectorAll('.placeholder-texte')
-    const contenantInput = document.querySelectorAll('.contenant-mot-clés')
-    const motCles = document.querySelectorAll('.mots-clés')
-    console.log(motCles)
-    input.forEach((el) => (el.style.width = '150px'))
-    document.addEventListener('click', (e) => {
+    const input = document.querySelectorAll(".mots-clés-input")
+    const arrowUp = document.querySelectorAll(".arrow-up")
+    const arrowDown = document.querySelectorAll(".arrow-down")
+    const texteInput = document.querySelectorAll(".placeholder-texte")
+    const contenantInput = document.querySelectorAll(".contenant-mot-clés")
+    const motCles = document.querySelectorAll(".mots-clés")
+  
+    input.forEach((el) => el.style.width = "150px")
+    document.addEventListener("click", (e) => {
         for (i = 0; i < input.length; i++) {
             // pour chaque index executer les commandes suivantes
-            if (e.target === input[i] && input[i].style.width === '150px') {
-                //console.log(e.target, input[i])
-                input[i].style.width = '800px'
-                arrowUp[i].style.display = 'none'
-                arrowDown[i].style.display = 'block'
-                texteInput[i].style.display = 'none'
-                input[i].setAttribute(
-                    'placeholder',
-                    'rechercher des' + texteInput[i].textContent + '...'
-                )
-                contenantInput[i].style.height = '375px'
-                motCles[i].style.display = 'flex'
-                afficheMotCles(motCles[i], data[i])
-            } else if (
-                e.target !== input[i] &&
-                input[i].style.width === '800px'
-            ) {
-                // console.log(e.target, input[i])
-                input[i].value = ' '
-                input[i].style.width = '150px'
-                arrowUp[i].style.display = 'block'
-                arrowDown[i].style.display = 'none'
-                texteInput[i].style.display = 'block'
-                input[i].setAttribute('placeholder', '')
-                contenantInput[i].style.height = '50px'
-                motCles[i].textContent = ''
-                motCles[i].style.display = 'none'
+            if (e.target === input[i] && input[i].style.width === "150px") {
+                console.log(arrowDown[i])
+                input[i].style.width = "800px"
+                arrowUp[i].style.display = "none"
+                arrowDown[i].style.display = "block"
+                //arrowUp[i].style.display = "block"
+                texteInput[i].style.display = "none"
+                input[i].setAttribute("placeholder", "rechercher des" + texteInput[i].textContent + "...")
+                contenantInput[i].style.height = "375px"
+                motCles[i].style.display = "flex"
+                afficheMotCles (motCles[i], data[i])
+            } else if (e.target !== input[i] && input[i].style.width === "800px") {
+               // console.log(e.target, input[i])
+                input[i].value = " "
+                input[i].style.width = "150px";
+                arrowUp[i].style.display = "block"
+                arrowDown[i].style.display = "none"
+                texteInput[i].style.display = "block"
+                contenantInput[i].style.height = "50px"
+                motCles[i].textContent = ""
+                motCles[i].style.display = "none"
             }
-            //
-            else if (
-                e.target === input[i] &&
-                input[i].style.width === '800px'
-            ) {
-                console.log(e.target, input[i])
-                e.target.value = ' '
-                input[i].style.width = '150px'
-                arrowUp[i].style.display = 'block'
-                arrowDown[i].style.display = 'none'
-                texteInput[i].style.display = 'block'
-                input[i].setAttribute('placeholder', '')
-                contenantInput[i].style.height = '50px'
-                motCles[i].textContent = ''
-                motCles[i].style.display = 'none'
+            // 
+            else if (e.target === input[i] && input[i].style.width === "800px") {
+                console.log(arrowUp[i])
+                e.target.value = " "
+                input[i].style.width = "150px";
+                arrowUp[i].style.display = "block"
+                arrowDown[i].style.display = "none"
+                texteInput[i].style.display = "block"
+                contenantInput[i].style.height = "50px"
+                motCles[i].textContent = ""
+                motCles[i].style.display = "none"
             }
         }
     })
-}
 
-function afficheMotCles(motCles, data) {
-    const newTab = data.filter((el) => data.indexOf(el) < 30) // n'affiche pas plus de 30 mot clès
+}
+function afficheMotCles (motCles, data) {
+
+    const newTab = data.filter(el => data.indexOf(el) < 30) // n'affiche pas plus de 30 mot clès
+    /* Pour chaque motCles présent dans le tablea de mot cles créer un nouveau mot cles das le conteneur de mots clés input */
     newTab.forEach((el) => {
-        const p = document.createElement('p')
+        const p = document.createElement("p")
         p.textContent = el
-        p.className = 'texte-mot-clés'
+        p.className = "texte-mot-clés"
         motCles.appendChild(p)
     })
+
 }
